4334b1a6e5a3d94824525007107df892e8c1e274
Eric Crawford <e2crawfo@uwaterloo.ca>
2013-04-24 17:35:50 -0400

    Added more tests
    * Added tests for the 3 calls for running entire test epochs,
    runBootstrap_jump, runBootstrap_hierarchical, runBootstrap_sentence.
    These currently do not test the output, they just call the functions,
    more of a test that they can run at all. Currently does both neural
    and non-neural, might want to take out the neural tests so it runs
    faster, since its not actually supposed to test the cleanup itself,
    just the the tests are structured properly.
    
    * Made better hierarchical test in test_neural_cleanup.py. Now uses
    the networkx python lib to create the graph, much less code.

---
+++ python/test/test_wordnet_assoc.py
@@ -0,0 +1,68 @@
+from ..wordnet_assoc_memory_tester import WordnetAssociativeMemoryTester
+from ..assoc_memory import AssociativeMemory
+from ..neural_assoc_memory import NeuralAssociativeMemory
+
+from .. import startup_utils
+from .. import symbol_definitions
+
+import unittest
+from nose.plugins.attrib import attr
+
+#for test selecting using Attrib nose plugin
+@attr(new=1)
+class TestWordnetAssoc(unittest.TestCase):
+
+  def createAssociator(self, id_vectors, semantic_pointers):
+    self.associator = AssociativeMemory(id_vectors, semantic_pointers, self.id, self.uv, self.ub, self.assoc_threshold)
+
+  def setUp(self):
+    self.id = False
+    self.uv = False
+    self.ub = False
+    self.threshold = 0.8
+    self.assoc_threshold = 0.3
+    seed = 10
+    dim = 512
+    prop = 0.1
+    input_dir, output_dir = startup_utils.read_config()
+    relation_symbols = symbol_definitions.uni_relation_symbols()
+
+    isA_symbols = symbol_definitions.isA_symbols()
+    sentence_symbols = symbol_definitions.sentence_role_symbols()
+
+    (self.corpus_dict, self.id_vectors, self.semantic_pointers) = \
+      startup_utils.setup_corpus(input_dir, relation_symbols, seed, dim, self.id, self.uv, prop) 
+
+    self.createAssociator(self.id_vectors, self.semantic_pointers)
+    self.tester = WordnetAssociativeMemoryTester(self.corpus_dict, self.id_vectors, self.semantic_pointers,
+        relation_symbols, self.associator, seed, output_dir, isA_symbols, sentence_symbols, self.uv, True)
+
+  def test_jump(self):
+    self.tester.runBootstrap_jump(2, 2)
+
+  def test_hierarchical(self):
+    self.tester.runBootstrap_hierarchical(2, 2)
+
+  def test_sentence(self):
+    self.tester.runBootstrap_sentence(2, 2)
+
+
+#for test selecting using Attrib nose plugin
+@attr(new=1)
+@attr(neural=1)
+class TestWordnetAssocNeural(TestWordnetAssoc):
+
+  def createAssociator(self, id_vectors, semantic_pointers):
+    self.associator = NeuralAssociativeMemory(id_vectors, semantic_pointers, self.id, self.uv, self.ub, self.assoc_threshold)
+
+  #def test_jump(self):
+  #  self.tester.runBootstrap_jump(2, 2)
+
+  #def test_hierarchical(self):
+  #  self.tester.runBootstrap_hierarchical(2, 2)
+
+  #def test_sentence(self):
+  #  self.tester.runBootstrap_sentence(2, 2)
+
+
+
