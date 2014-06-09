c88bf59d28b0231cf77f5d785b31be1a537ce9e8
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-06-09 12:05:40 -0400

    dodo.py can now run experiments and create plots.

--- python/dodo.py
+++ python/dodo.py
@@ -1,40 +1,60 @@
 # dodo.py
-
 import datetime
 import string
 import copy
 import os
+from doit import get_var
+from doit.tools import run_once
 
 import run
+import plot
 from mytools.bootstrap import Bootstrapper
 
 DOIT_CONFIG = {'verbosity': 2}
 
-num_subtasks = 3
+if __name__ != "__main__":
+    DOIT_CONFIG['datetime'] = get_var('datetime', '')
+
+num_subtasks = 4
 
 test_names = ['jump', 'hierarchical', 'deep']
 
-date_time_string = str(datetime.datetime.now()).split('.')[0]
-date_time_string = reduce(
-    lambda y, z: string.replace(y, z, "_"),
-    [date_time_string, ":", " ", "-"])
+date_time_string = DOIT_CONFIG.get('datetime', '')
 
-# directory = '/data/e2crawfo/hrr-scaling/experiment'
-directory = ('/home/e2crawfo/hrr-scaling/experiments/experiment_'
+if not date_time_string:
+    date_time_string = str(datetime.datetime.now()).split('.')[0]
+    date_time_string = reduce(
+        lambda y, z: string.replace(y, z, "_"),
+        [date_time_string, ":", " ", "-"])
+
+# experiment_directory = '/home/e2crawfo/hrr-scaling/experiments'
+experiment_directory = '/data/e2crawfo/hrr-scaling/experiments'
+
+directory = (experiment_directory+'/experiments_'
              + date_time_string)
 
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-args = {'num_runs': 2, 'jump_trials': 3, 'hier_trials': 3, 'sent_trials': 0,
-        'deep_trials': 3, 'expr': 0, 'unitary_roles': True,
-        'short_sentence': False, 'do_neg': True, 'corpus_seed': 100,
-        'extractor_seed': 200, 'test_seed': 300, 'seed': None,
-        'dimension': 512, 'num_synsets': 100, 'proportion': 1.0,
-        'unitary_relations': False, 'abstract': True,
-        'synapse': 0.005, 'timesteps': 100, 'threshold': 0.3,
+args = {'num_runs': 5, 'jump_trials': 100, 'hier_trials': 20, 'sent_trials': 0,
+        'deep_trials': 30, 'expr': 0, 'unitary_roles': True,
+        'short_sentence': False, 'do_neg': True, 'corpus_seed': -1,
+        'extractor_seed': -1, 'test_seed': -1, 'seed': 1000,
+        'dimension': 512, 'num_synsets': -1, 'proportion': 1.0,
+        'unitary_relations': False, 'abstract': False,
+        'synapse': 0.005, 'timesteps': 75, 'threshold': 0.3,
         'probeall': False, 'identical': True, 'fast': False, 'plot': True,
         'show': False, 'gpus': [], 'ocl': [], 'outfile_format': ""}
+
+neural_summary = directory + '/neural_summary'
+abstract_summary = directory + '/abstract_summary'
+
+results_files = []
+for s in ['abstract', 'neural']:
+    for st in range(num_subtasks):
+        for tn in test_names:
+            fn = '%s/%s_%s_subtask_%g' % (directory, s, tn, st)
+            results_files.append(fn)
 
 
 def consolidate_bootstraps(input_filenames, summary_filename):
@@ -47,33 +67,154 @@ def consolidate_bootstraps(input_filenames, summary_filename):
     bs.print_summary(summary_filename)
 
 
-def task_experiment():
-    results_files = []
+def task_neural_experiments():
 
     for subtask in range(num_subtasks):
 
-        filename_format = '%s/%%s_subtask_%g' % (directory, subtask)
+        filename_format = '%s/neural_%%s_subtask_%g' % (directory, subtask)
 
         subtask_args = copy.deepcopy(args)
         subtask_args['outfile_format'] = filename_format
+        subtask_args['seed'] += subtask * 1000
         subtask_args['gpus'] = [subtask]
+        subtask_args['abstract'] = False
 
         targets = [filename_format % t for t in test_names]
-        results_files.extend(targets)
+
+        print targets
 
         yield {
             'name': 'Subtask %g' % subtask,
             'actions': [(run.run, [], subtask_args)],
             'file_dep': [],
             'targets': targets,
+            'uptodate': [run_once]
             }
 
-    experiment_file = directory + '/summary'
+    neural_results_files = filter(lambda x: 'neural' in x, results_files)
 
     yield {
         'name': 'Consolidate',
         'actions': [(consolidate_bootstraps,
-                     [results_files, experiment_file])],
-        'file_dep': results_files,
-        'targets': [experiment_file]
+                     [neural_results_files, neural_summary])],
+        'file_dep': neural_results_files,
+        'targets': [neural_summary]
         }
+
+
+def task_abstract_experiments():
+    for subtask in range(num_subtasks):
+
+        filename_format = '%s/abstract_%%s_subtask_%g' % (directory, subtask)
+
+        subtask_args = copy.deepcopy(args)
+        subtask_args['outfile_format'] = filename_format
+        subtask_args['seed'] += subtask * 1000
+        subtask_args['abstract'] = True
+
+        targets = [filename_format % t for t in test_names]
+
+        yield {
+            'name': 'Subtask %g' % subtask,
+            'actions': [(run.run, [], subtask_args)],
+            'file_dep': [],
+            'targets': targets,
+            'uptodate': [run_once]
+            }
+
+    abstract_results_files = filter(lambda x: 'abstract' in x, results_files)
+
+    yield {
+        'name': 'Consolidate',
+        'actions': [(consolidate_bootstraps,
+                     [abstract_results_files, abstract_summary])],
+        'file_dep': abstract_results_files,
+        'targets': [abstract_summary]
+        }
+
+
+def plot_results(summary_filenames, keys, **kwargs):
+    """
+    Read the summary bootstrap files and create a plot using
+    plot_performance.
+
+    Params:
+
+    summary_filenames: list of str
+        The names of the summary bootstrap files, with the data
+        from all experiments.
+    keys: list of str
+        The keys to extract from the summary bootstrap
+
+    **kwargs is passed to plot_performance
+
+    """
+
+    bs = Bootstrapper(write_raw_data=True)
+
+    means = []
+    low_cis = []
+    high_cis = []
+
+    for fn in summary_filenames:
+        bs.read_bootstrap_file(fn)
+
+        mean = []
+        low_ci = []
+        high_ci = []
+
+        for key in keys:
+            stats = bs.get_stats(key)
+
+            mean.append(stats[1] * 100)
+            low_ci.append(stats[2][0] * 100)
+            high_ci.append(stats[2][1] * 100)
+
+        means.append(mean)
+        low_cis.append(low_ci)
+        high_cis.append(high_ci)
+
+    plot.plot_performance(means, low_cis, high_cis, **kwargs)
+
+
+def task_performance_plot():
+    summary_filenames = [abstract_summary, neural_summary]
+    plot_filename = directory + '/prgraph.pdf'
+
+    kwargs = {
+        'summary_filenames': [abstract_summary, neural_summary],
+
+        'keys': ['jump_score_correct', 'hierarchical_score',
+                 'sentence_score_1', 'sentence_score_2'],
+
+        'measure_labels': ["Simple", "Hierarchical",
+                           "Sentence\n(Surface)",
+                           "Sentence\n(Embedded)"],
+
+        'condition_labels': ["Abstract", "Neural"],
+
+        'filename': plot_filename
+        }
+
+    yield {
+        'name': 'performance_graph',
+        'actions': [(plot_results, [], kwargs)],
+        'file_dep': summary_filenames,
+        'targets': [plot_filename]
+        }
+
+
+def task_tuning_curve_plot():
+
+    filename = 'tuning_curves.pdf'
+    yield {
+        'actions': [(plot.plot_tuning_curves, [filename])],
+        'targets': filename
+        }
+
+# if __name__ == '__main__':
+#     task_iter = task_neural_experiments()
+#     for task in task_iter:
+#         actions = task['actions']
+#         for action in actions:
+#            action[0](*action[1], **action[2])
--- python/plot.py
+++ python/plot.py
@@ -1,116 +1,281 @@
-from random import *
-
 import argparse
-parser = argparse.ArgumentParser(description="Draw a bar graph")
-parser.add_argument('-s', action='store_true', help='Supply this argument to save the graph')
-argvals = parser.parse_args()
-
-save = argvals.s
-
-try:
-  import matplotlib as mpl
-  if save:
-    mpl.use('Agg')
-
-  import matplotlib.pyplot as plt
-except:
-  pass
-
-#mpl.rc('font', family='serif', serif='Times New Roman')
-#mpl.rcParams['lines.linewidth'] = 0.3
-
-import numpy
-from operator import *
-
-fig = plt.figure(figsize=(5,2.55))
-
-mpl.rcParams.update({'font.size': 7})
-
-#this is the number of different bar locations...typically each location is a measure
-num_vals = 4
-
-#this is the number of bars at each location...typically  series of conditions that we want to compare on multiple measures
-bars_per_val = 2
-
-#condition (down) by measure(across)
-y_vals = [[99.25, 96.125, 86.7611, 55.24722],
-          [100.0, 95.5, 99.6, 99.6]]
-error_lo = [[98.75, 94.375, 85.5888, 53.486],
-            [100.0, 94.3, 99.3, 99.2]]
-error_hi = [[99.7, 97.75, 88.069, 56.986],
-            [100.0, 96.9, 99.8, 99.7]]
-
-#y_vals = [[97.7, 99.625, 99.85556, 99.88556],
-#          [100.0, 95.5, 99.6, 99.6]]
-#error_lo = [[97.1, 99.25, 99.7, 99.73888],
-#            [100.0, 94.3, 99.3, 99.2]]
-#error_hi = [[98.3, 100.0, 99.9, 99.96666],
-#            [100.0, 96.9, 99.8, 99.7]]
-
-#, [17.9, 74.9, 51.8], [81.0, 96.4, 93.2]]
-#[16.2, 71.5, 50.3], [79.4, 95.4, 91.9]]
-#, [19.7, 78.1, 53.3], [82.6, 97.5, 94.5]]
-
-tick_labels = ["Simple", "Hierarchical", "Sentence\n(Surface)", "Sentence\n(Embedded)"]
-measure_labels = ["Abstract", "Neural"]
-
-error_lo = [[y - el for y, el in zip(yvl, ell)] for yvl, ell in zip(y_vals, error_lo)]
-error_hi = [[eh - y for y, eh in zip(yvl, ehl)] for yvl, ehl in zip(y_vals, error_hi)]
 
 
-hatch = ['/', '|||||', '\\', 'xxx', '||', '--', '+', 'OO', '...', '**']
-color = [[0.33] * 3, [0.5] * 3, [0.66] * 3]
+def plot_performance(y_vals, error_lo, error_hi, measure_labels,
+                     condition_labels, filename='', show=False, colors=None,
+                     **kwargs):
+    """
+    Plot bar chart with error bars. A measure is some specific dimension that
+    we want to compare different conditions on. A condition is some
+    configuration of parameters that we want to compare on different measures.
 
-cross_measurement_spacing = 0.4
-within_measurement_spacing = 0.0
-bar_width = 0.4
+    E.g. two conditions could be 'model' and 'experiment', and the measures
+    would be aspects of the performance that we want to compare them on.
 
-#plt.figure(figsize=(4,2))
-#plt.title("Model Performance")
-plt.ylabel("% Correct")
+    In the bar chart, each condition is assigned a color. Each measure gets a
+    locations on the graph, where bars from each condition will be displayed.
+    """
 
-bar_left_positions = [[] for b in range(bars_per_val)]
-val_offset = 0
-middles = []
-for i in range(num_vals):
-  val_offset += cross_measurement_spacing
+    import matplotlib as mpl
+    if show:
+        mpl.use('Qt4Agg')
+    else:
+        mpl.use('Agg')
 
-  left_side = val_offset
+    import matplotlib.pyplot as plt
 
-  for j in range(bars_per_val):
-    if j > 0:
-      val_offset += within_measurement_spacing
-      val_offset += bar_width
+    assert(len(y_vals[0]) ==
+           len(error_lo[0]) ==
+           len(error_hi[0]))
 
-    bar_left_positions[j].append(val_offset)
+    assert(len(y_vals) ==
+           len(error_lo) ==
+           len(error_hi))
 
-  val_offset += bar_width
+    # this is the number of different bar locations
+    # typically each location is a measure
+    try:
+        num_measures = len(y_vals[0])
+    except IndexError:
+        raise ValueError("y_vals must be a 2D array/list")
 
-  right_side = val_offset
+    # this is the number of bars at each location...typically a set of
+    # conditions that we want to compare on multiple measures
+    try:
+        num_conditions = len(y_vals)
+    except Exception:
+        raise ValueError("y_vals must be a 2D array/list")
 
-  middles.append(float(left_side + right_side) / 2.0)
+    if not measure_labels:
+        measure_labels = [''] * num_measures
 
-for blp, yv, cl, el, eh, lb in zip(bar_left_positions, y_vals, color, error_lo, error_hi, measure_labels):
-  plt.bar(blp, yv, color=cl, width = bar_width, linewidth=0.2, yerr=[el, eh], ecolor="black", label=lb, error_kw = {"linewidth":0.5, "capsize":2.0})
+    if not condition_labels:
+        condition_labels = [''] * num_conditions
 
-legend = plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size':9}, handlelength=.75, handletextpad=.5, shadow=False, frameon=False)
+    assert(len(measure_labels) == num_measures)
+    assert(len(condition_labels) == num_conditions)
 
-ax = fig.axes[0]
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-handles, labels = ax.get_legend_handles_labels()
+    # hatch = ['/', '|||||', '\\', 'xxx', '||', '--', '+', 'OO', '...', '**']
+    if not colors:
+        colors = [[0.33] * 3, [0.5] * 3, [0.66] * 3]
 
-ticks = middles
+    cross_measurement_spacing = 0.4
+    within_measurement_spacing = 0.0
+    bar_width = 0.4
 
-plt.xticks(ticks, tick_labels)
-plt.ylim([0.0, 105.0])
-plt.xlim([0.0, val_offset + cross_measurement_spacing])
-plt.axhline(100.0, linestyle='--', color='black')
+    error_lo = [[y - el for y, el in zip(yvl, ell)]
+                for yvl, ell in zip(y_vals, error_lo)]
+    error_hi = [[eh - y for y, eh in zip(yvl, ehl)]
+                for yvl, ehl in zip(y_vals, error_hi)]
 
-plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
-plt.show()
+    # mpl.rc('font', family='serif', serif='Times New Roman')
+    # mpl.rcParams['lines.linewidth'] = 0.3
 
-if save:
-  plt.savefig('prgraph.pdf')
+    fig = plt.figure(figsize=(5, 2.55))
+
+    mpl.rcParams.update({'font.size': 7})
+
+    # plt.title("Model Performance")
+    plt.ylabel("% Correct")
+
+    bar_left_positions = [[] for b in range(num_conditions)]
+    val_offset = 0
+    middles = []
+    for i in range(num_measures):
+        val_offset += cross_measurement_spacing
+
+        left_side = val_offset
+
+        for j in range(num_conditions):
+            if j > 0:
+                val_offset += within_measurement_spacing
+                val_offset += bar_width
+
+            bar_left_positions[j].append(val_offset)
+
+        val_offset += bar_width
+
+        right_side = val_offset
+
+        middles.append(float(left_side + right_side) / 2.0)
+
+    zipped = zip(
+        bar_left_positions, y_vals, colors,
+        error_lo, error_hi, condition_labels)
+
+    for blp, yv, cl, el, eh, lb in zipped:
+        plt.bar(
+            blp, yv, color=cl, width=bar_width, linewidth=0.2,
+            yerr=[el, eh], ecolor="black", label=lb,
+            error_kw={"linewidth": 0.5, "capsize": 2.0})
+
+    plt.legend(
+        loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 9},
+        handlelength=.75, handletextpad=.5, shadow=False, frameon=False)
+
+    ax = fig.axes[0]
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    handles, labels = ax.get_legend_handles_labels()
+
+    ticks = middles
+
+    plt.xticks(ticks, measure_labels)
+    plt.ylim([0.0, 105.0])
+    plt.xlim([0.0, val_offset + cross_measurement_spacing])
+    plt.axhline(100.0, linestyle='--', color='black')
+
+    plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
+
+    if filename:
+        plt.savefig(filename)
+
+    if show:
+        plt.show()
 
 
+def plot_tuning_curves(filename, plot_decoding=False, show=False):
+    """
+    Plot tuning curves for an association population and for a standard
+    subpopulation (of the neural extraction network).
+    """
+
+    import nengo
+    from nengo.utils.distributions import Uniform
+    from nengo.utils.ensemble import tuning_curves
+    import numpy as np
+    import matplotlib.gridspec as gridspec
+    import matplotlib as mpl
+    if show:
+        mpl.use('Qt4Agg')
+    else:
+        mpl.use('Agg')
+
+    import matplotlib.pyplot as plt
+
+    neurons_per_item = 20
+    neurons_per_dim = 50
+    intercepts_low = 0.29
+    intercepts_range = 0.00108
+
+    intercepts = Uniform(intercepts_low, intercepts_low + intercepts_range)
+
+    tau_rc = 0.034
+    tau_ref = 0.0026
+    radius = 1.0
+    encoders = np.ones((neurons_per_item, 1))
+
+    threshold = 0.3
+    threshold_func = lambda x: 1 if x > threshold else 0
+
+    max_rates = Uniform(200, 350)
+
+    model = nengo.Network("Associative Memory")
+    with model:
+        neurons = nengo.LIF(neurons_per_item,
+                            tau_rc=tau_rc,
+                            tau_ref=tau_ref)
+
+        assoc = nengo.Ensemble(neurons, 1, intercepts=intercepts,
+                               encoders=encoders, label="assoc",
+                               radius=radius, max_rates=max_rates)
+
+        n_eval_points = 750
+        eval_points = np.random.normal(0, 0.06, (n_eval_points, 1))
+        eval_points.T[0].sort()
+        radius = 5.0 / np.sqrt(512)
+        standard = nengo.Ensemble(nengo.LIF(neurons_per_dim), 1,
+                                  eval_points=eval_points,
+                                  radius=radius)
+
+        if plot_decoding:
+            dummy = nengo.Ensemble(nengo.LIF(1), 1)
+            conn = nengo.Connection(assoc, dummy, function=threshold_func)
+            dummy2 = nengo.Ensemble(nengo.LIF(1), 1)
+            conn2 = nengo.Connection(standard, dummy2)
+
+    sim = nengo.Simulator(model)
+
+    if plot_decoding:
+        gs = gridspec.GridSpec(3, 2)
+    else:
+        gs = gridspec.GridSpec(2, 2)
+
+    plt.subplot(gs[0:2, 0])
+
+    assoc_eval_points, assoc_activities = tuning_curves(assoc, sim)
+
+    for neuron in assoc_activities.T:
+        plt.plot(assoc_eval_points.T[0], neuron)
+    plt.title("Association Subpopulation")
+    plt.ylabel("Firing Rate (spikes/s)")
+    plt.xlabel("ex")
+    plt.ylim((0, 400))
+
+    plt.subplot(gs[0:2, 1])
+
+    # We want different eval points for display purposes than for
+    # optimization purposes
+    eval_points = Uniform(-radius, radius).sample(n_eval_points)
+    eval_points.sort()
+    eval_points = eval_points.reshape((n_eval_points, 1))
+
+    # have to divide by radius on our own since tuning_curves skips that step
+    _, activities = tuning_curves(standard, sim, eval_points/radius)
+    for neuron in activities.T:
+        plt.plot(eval_points, neuron)
+
+    plt.title("Standard Subpopulation")
+    plt.xlabel("ex")
+    plt.xlim((-radius, radius))
+    plt.ylim((0, 400))
+
+    if plot_decoding:
+        plt.subplot(gs[2, 0])
+        decoders = sim.data[conn].decoders
+        plt.plot(assoc_eval_points.T[0],
+                 0.001 * np.dot(assoc_activities, decoders.T))
+        plt.axhline(y=1.0, ls='--')
+
+        plt.subplot(gs[2, 1])
+        x, activities2 = tuning_curves(standard, sim, assoc_eval_points/radius)
+        decoders = sim.data[conn2].decoders
+        plt.plot(
+            assoc_eval_points.T[0],
+            0.001 * np.dot(activities2, decoders.T))
+        plt.plot([-1.0, 1.0], [-1.0, 1.0], c='k', ls='--')
+        plt.axvline(x=radius, c='k', ls='--')
+        plt.axvline(x=-radius, c='k', ls='--')
+
+    plt.tight_layout()
+
+    if filename:
+        plt.savefig(filename)
+
+    if show:
+        plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Draw a bar graph")
+
+    parser.add_argument(
+        '-s', action='store_true',
+        help='Supply this argument to save the graph')
+
+    argvals = parser.parse_args()
+
+    if argvals.s:
+        filename = 'prgraph.pdf'
+    else:
+        filename = ''
+
+    # condition (down) by measure(across)
+    y_vals = [[99.25, 96.125, 86.7611, 55.24722],
+              [100.0, 95.5, 99.6, 99.6]]
+    error_lo = [[98.75, 94.375, 85.5888, 53.486],
+                [100.0, 94.3, 99.3, 99.2]]
+    error_hi = [[99.7, 97.75, 88.069, 56.986],
+                [100.0, 96.9, 99.8, 99.7]]
+
+    plot_performance(y_vals, error_lo, error_hi, filename=filename, show=True)
